1bfbcaaa1d523437b91b298aec7385f155b35880
Dayse Soares <daysedesign@gmail.com>
2020-08-22 16:17:42 -0300

    Salvando novo cliente

--- src/interceptors/error-iterceptor.ts
+++ src/interceptors/error-iterceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular";
+import { FieldMessage } from "../models/fieldmessage";
 
 
 @Injectable()
@@ -34,6 +35,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                                 case 401:
                                     this.handle401();
                                     break;
+                                case 422:
+                                    this.handle422(errorObj);
+                                    break;                                
                                 default:
                                     this.handleDefaultError(errorObj);
                                     break;
@@ -59,6 +63,18 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+        });
+        alert.present();
+    }
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create({
             title: 'Erro '+errorObj.status +': '+errorObj.error,
@@ -69,6 +85,14 @@ export class ErrorInterceptor implements HttpInterceptor {
             ]
         });
         alert.present();
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 
 }
