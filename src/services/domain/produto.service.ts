7fa2debb95968072d846bc33c21463d8e0f0ee82
Dayse Soares <daysedesign@gmail.com>
2020-08-24 21:29:25 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Observable } from 'rxjs/Rx';
+import { ProdutoDTO } from "../../models/produto.dto";
 
 
 @Injectable()
@@ -13,10 +14,21 @@ export class ProdutoService {
 	findByCategoria(categoria_id : string) {
 		return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
+
+    findById(produto_id : string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
     
     getSmallImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
         return this.http.get(url, {responseType : 'blob'});    
     }
+
+    getImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
+        return this.http.get(url, {responseType : 'blob'});    
+    }
+
+    
 
 }
