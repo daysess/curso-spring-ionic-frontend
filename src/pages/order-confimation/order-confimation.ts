7bb19ad611d656b716ce4ec4223313840aa3cf1d
Dayse Soares <daysedesign@gmail.com>
2020-09-01 22:10:21 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confimation/order-confimation.ts
+++ src/pages/order-confimation/order-confimation.ts
@@ -19,6 +19,7 @@ export class OrderConfimationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -57,17 +58,26 @@ export class OrderConfimationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
