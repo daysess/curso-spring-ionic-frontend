22f13d4fcfed3589d3e322fddc53691b28ea27e2
Dayse Soares <daysedesign@gmail.com>
2020-09-02 21:01:44 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -16,16 +16,21 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
+    let loader = this.presentLoading();
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        loader.dismiss();
         this.loadImageUrls();
-      }, error => {});
+      }, error => {
+        loader.dismiss();
+      });
 
   }
 
@@ -42,6 +47,15 @@ export class ProdutosPage {
 
   showDetail(produto_id : string){
     this.navCtrl.push('ProdutoDetailPage', { produto_id : produto_id });
+  }
+
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+      //duration: 3000
+    });
+    loader.present();
+    return loader;
   }
 
 }
